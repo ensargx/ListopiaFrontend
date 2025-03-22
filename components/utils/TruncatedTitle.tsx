1b23f1e53d809d8d424d7d39ee10f543ef4ee37b
orcnnn <orcun.aydin@std.yildiz.edu.tr>
2025-03-23 00:06:49 +0300

    Card Center problem fixed

--- components/utils/TruncatedTitle.tsx
+++ components/utils/TruncatedTitle.tsx
@@ -4,7 +4,7 @@ import * as React from "react";
 export const TruncatedTitle = ({ title, maxLength }: { title: string, maxLength: number }) => {
     const truncatedTitle = title.length > maxLength ? title.slice(0, maxLength) + '...' : title;
     return (
-        <CardDescription className="flex text-xl font-bold m-2 px-6 w-full truncate justify-items-center">
+        <CardDescription className="text-xl font-bold m-2 px-6 w-full truncate justify-items-center">
             {truncatedTitle}
         </CardDescription>
     );
