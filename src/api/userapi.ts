1141f855f9d63e14f28823c045e55a91b747c03b
orcnnn <orcun.aydin@std.yildiz.edu.tr>
2025-04-27 15:50:19 +0300

    Message popup screen fixed

--- src/api/userapi.ts
+++ src/api/userapi.ts
@@ -1,11 +1,12 @@
 // src/app/api_/userapi.ts
 import { User } from "@/types/user";
 import {PaginatedResponse} from "@/types/friends"
+import { apiFetch as fetch } from './apiClient';
 
 const BASE = "https://api.ensargok.com/api/v1";
 
 ////
-// @TODO: DO BETTER ERROR HANDLING AND TYPES 
+// @TODO: DO BETTER ERROR HANDLING AND TYPES
 ////
 
 export default interface APIResponse {
@@ -70,7 +71,7 @@ export async function fetchUserByUsername(
     username: string
 ): Promise<User> {
     const res = await fetch(
-        `${BASE}/user/${encodeURIComponent(username)}`
+        `${BASE}/user/username/${encodeURIComponent(username)}`
     );
     if (!res.ok) {
         const errorText = await res.text();
@@ -79,24 +80,99 @@ export async function fetchUserByUsername(
     return res.json();
 }
 
-
-export async function fetchFriendsByUUID(
-    uuid: string,
-    pageNumber: number,
-    pageSize: number
-): Promise<PaginatedResponse<User>> {
+export async function changeUsernameRequest(username: string) : Promise<APIResponse> {
     const res = await fetch(
-        `/api/v1/user/${encodeURIComponent(uuid)}/friends` +
-        `?pageNumber=${pageNumber}&pageSize=${pageSize}`,
-        {
-            headers: { "Content-Type": "application/json" },
-            credentials: "include"
+        `${BASE}/user/username`, {
+            method: "PUT",
+            credentials: "include",
+            body: JSON.stringify({
+                newUsername: username
+            })
         }
     );
-    if (!res.ok) throw new Error('Failed to fetch friends list');
+    if (!res.ok) throw new Error('Could not change username');
     return res.json();
 }
 
+export async function changePasswordRequest(password: string) : Promise<APIResponse> {
+    const res = await fetch(
+        `${BASE}/user/password`, {
+            method: "PUT",
+            credentials: "include",
+            body: JSON.stringify({
+                password: password
+            })
+        }
+    );
+    if (!res.ok) throw new Error('Could not change password');
+    return res.json();
+}
+
+export async function changeBiographyRequest(biography: string) : Promise<APIResponse> {
+    const res = await fetch(
+        `${BASE}/user/biography`, {
+            method: "PUT",
+            credentials: "include",
+            body: JSON.stringify({
+                biography: biography
+            })
+        }
+    );
+    if (!res.ok) throw new Error('Could not change biography');
+    return res.json();
+}
+
+export async function addFriendRequest(uuid: string) : Promise<APIResponse> {
+    const res = await fetch(
+        `${BASE}/user/friend/add/${uuid}`, {
+            method: "POST",
+            credentials: "include",
+        }
+    );
+    if (!res.ok) throw new Error('Could not change password');
+    return res.json();
+}
+
+export async function acceptFriendRequest(uuid: string) : Promise<APIResponse> {
+    const res = await fetch(
+        `${BASE}/user/friend/accept/${uuid}`, {
+            method: "POST",
+            credentials: "include",
+        }
+    );
+    if (!res.ok) throw new Error('Could not change password');
+    return res.json();
+}
+
+export async function rejectFriendRequest(uuid: string) : Promise<APIResponse> {
+    const res = await fetch(
+        `${BASE}/user/friend/reject/${uuid}`, {
+            method: "POST",
+            credentials: "include",
+        }
+    );
+    if (!res.ok) throw new Error('Could not change password');
+    return res.json();
+}
+
+export async function removeFriendRequest(uuid: string) : Promise<APIResponse> {
+    const res = await fetch(
+        `${BASE}/user/friend/remove/${uuid}`, {
+            method: "DELETE",
+            credentials: "include",
+        }
+    );
+    if (!res.ok) throw new Error('Could not change password');
+    return res.json();
+}
+
+export async function fetchFriendsByUUID(
+    uuid: string
+): Promise<PaginatedResponse<User>> {
+    const res = await fetch(`${BASE}/user/uuid/${encodeURIComponent(uuid)}/friends`);
+    if (!res.ok) throw new Error('Arkadaş listesi alınamadı');
+    return res.json();
+}
 
 export async function fetchUserMe(): Promise<User>{
     const res = await fetch(`${BASE}/user/me`, {
@@ -110,35 +186,20 @@ export async function fetchUserMe(): Promise<User>{
     return res.json();
 }
 
+// aşağıdaki fonksiyonlar yanlış silin.
+
 export async function addFriend(
+    userUuid: string,
     friendUuid: string
-): Promise<APIResponse> {
+): Promise<void> {
     const res = await fetch(
-        `${BASE}/user/add_friend`,
+        `${BASE}/user/${encodeURIComponent(userUuid)}/friends`,
         {
             method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ uuid: friendUuid }),
-            credentials: "include"
+            body: JSON.stringify({ friendUuid }),
         }
     );
     if (!res.ok) throw new Error("Failed to add friend");
-    return res.json();
-}
-export async function fetchFriendRequests(
-    pageNumber: number,
-    pageSize: number
-): Promise<PaginatedResponse<User>> {
-    const res = await fetch(
-        `/api/v1/user/friend_requests` +
-        `?pageNumber=${pageNumber}&pageSize=${pageSize}`,
-        {
-            headers: { "Content-Type": "application/json" },
-            credentials: "include"
-        }
-    );
-    if (!res.ok) throw new Error('Failed to fetch friend requests list');
-    return res.json();
 }
 
 export async function removeFriend(
